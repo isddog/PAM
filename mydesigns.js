ef40fb79d188749087e1a42f5f0e0edb02661159
isddog <51778119+isddog@users.noreply.github.com>
2019-06-15 22:47:53 +0900

    Update mydesigns.js

--- mydesigns.js
+++ mydesigns.js
@@ -1,25 +1,39 @@
-const colorPicker = document.getElementById("colorPicker").value; // Select color input
-const sizePicker = document.getElementById("sizePicker"); // Select size input
+//Entering whole numbers in the "Height" and "Width" fields, and submitting the form, causes an empty grid to appear on the page.
+
+const colorPicker = document.getElementById("colorPicker").value; 
+const sizePicker = document.getElementById("sizePicker"); 
 const gridCanvas = document.getElementById("pixelCanvas"); 
-const gridHeight = document.getElementById("inputHeight").value;
-const gridWidth = document.getElementById("inputWidth").value;
+const gridHeight = document.getElementById("inputHeight");
+const gridWidth = document.getElementById("inputWidth");
 
-// When size is submitted by the user, call makeGrid()
+//Choosing a color from the 'color selector' and clicking on a grid square causes that grid square (and only that square) to change color.
 
-function makeGrid(gridHeight, gridWidth) {
-    rowCount = gridHeight;
-    cellCount = gridWidth;
-    for (r = 0; r < rowCount; r++) {
-        var newRow = document.createElement("tr");
-        gridCanvas.appendChild(newRow);
-    }
-    for (c = 0; c < cellCount; c++) {
-        var cell = document.createElement("td");
-        newRow.appendChild(cell);
+function makeGrid() {
+    gridCanvas.innerHTML = "";
+    var rowCount = gridHeight.value;
+    var cellCount = gridWidth.value;
+    for (let r = 0; r < rowCount; r++) {
+        var tr = document.createElement("tr");
+        gridCanvas.appendChild(tr);
+        var newRow = gridCanvas.insertRow(r);
+    
+        for (let c = 0; c < cellCount; c++) {
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.addEventListener('click', fillGrid); //Event listeners are properly added to the grid squares (and not to the border or the table itself).
+            var cell = newRow.insertCell(c);
+            cell.addEventListener('click', fillGrid);
+        }
     }
 }
 
-sizePicker.addEventListener('submit', function(event) {
-    event.preventDefault();
-    makeGrid(gridHeight, gridWidth);
-}, false);
+// If there are already colored squares in the grid, clicking the Submit button clears them out.
+
+function fillgrid() {
+    fillGrid.setAttribute('style', 'bgcolor: {color.value}' )
+    fillGrid.addEventListener('submit', function (event) {
+        makeGrid();
+        event.preventDefault();
+        gridCanvas.removeChild(gridCanvas.firstChild);
+    })
+}
